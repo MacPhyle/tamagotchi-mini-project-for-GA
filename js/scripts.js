3cb80b8089f5ab0b1a65777138de7a355294d910
MacPhyle <macphyle@gmail.com>
2024-04-20 20:50:23 -0700

    Set up an object for my tamagotchi.

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+// testing files are linked
+function testingLink() {
+    console.log("This file is linked!");
+}
+testingLink();
+
+// colors for eyes and fur
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
+// let randomColor = 
+
+// game object or class
+const tamagotchi = {
+    name: "",
+    age: 0,
+    // using health rather than hunger, because it makes more sense in gaming
+    health: 10,
+    fatigue: 0,
+    boredom: 0,
+    FurColor: randomColor,
+    eyesColor: randomColor
+};
+    // event listeners/handlers that call methods
