89102e18e797c044061ae0c3cae485987c2781dd
MacPhyle <macphyle@gmail.com>
2024-04-24 03:03:24 -0700

    Got my character visible on the page.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 // testing files are linked
-function testingLink() {
-    console.log("This file is linked!");
-}
-testingLink();
+// function testingLink() {
+//     console.log("This file is linked!");
+// }
+// testingLink();
 
 // colors for eyes and fur
-const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+// const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 
 // let randomColor = 
 
@@ -13,11 +13,11 @@ const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 const tamagotchi = {
     name: "",
     age: 0,
-    // using health rather than hunger, because it makes more sense in gaming
     health: 10,
+    hunger: 0,
     fatigue: 0,
     boredom: 0,
-    FurColor: randomColor,
-    eyesColor: randomColor
+    // FurColor: randomColor,
+    // eyesColor: randomColor
 };
     // event listeners/handlers that call methods
