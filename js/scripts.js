2a8ca6ced1301807895213bf7592d51cf3831899
MacPhyle <macphyle@gmail.com>
2024-04-28 17:55:41 -0700

    More work on the stats plus got some CSS issues fixed

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,47 @@ const tamagotchi = {
     boredom: 0,
     // FurColor: randomColor,
     // eyesColor: randomColor
-};
+}
 
+const $h1 = $("h1");
+const $h2 = $("h2");
+const $h3 = $("ul");
+const $li = $("li");
 
-    // event listeners/handlers that call methods
+$("h1").css("font-family","bree","sans-serif")
+$("h1").css({"font-size": "80px", "color": "white", "font-weight": "800", "font-style": "normal", "line-height": .5})
+
+$("h2").css("font-family","parlare")
+$("h2").css({"font-weight": "400", "font-style": "normal", "color": "#fff2cc", "text-align": "left", "padding-left": "145px", "font-size": "36px", "line-height": "20%"})
+
+$("ul").css("font-family","bree","sans-serif")
+$("ul").css({"font-size": "16px", "text-align": "center", "padding-right": "350px", "padding-left": "100px", "color": "white", "list-style-type": "none", "font-weight": "200"})
+
+$(".petName").css("font-family", "bree", "sans-serif")
+$(".petName").css({"font-size": "16px", ""})
+
+$("li").css({"display": "inline", "padding-left": "10px", "padding-right": "10px"})
+
+// h2 {
+//     font-family: "parlare", sans-serif;
+//     font-weight: 400;
+//     font-style: normal;
+//     color: #fff2cc;
+//      font-size: 50px;
+//     padding-left: 290px;
+//     line-height: 20%;
+//     margin-right: auto;
+// }
+
+console.log($("statsList.li"));
+
+// h1 {
+//     font-size: 100px;
+//     color: white;
+//     font-family: "bree", sans-serif;
+//     font-weight: 800;
+//     font-style: normal;
+//     margin-left: auto;
+// }
+
+// event listeners/handlers that call methods
